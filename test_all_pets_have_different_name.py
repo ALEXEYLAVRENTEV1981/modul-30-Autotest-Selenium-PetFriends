07000a91b2f0c81a316f408d6dbbff8292ea6fd4
Alexei <Lavrentev.Alex@mail.ru>
2023-07-24 19:16:42 +0700

    Update test_all_pets_have_different_name.py

--- test_all_pets_have_different_name.py
+++ test_all_pets_have_different_name.py
@@ -28,5 +28,3 @@ def test_all_pets_have_different_names(go_to_my_pets):
    assert r == 0
    print(r)
    print(pets_name)
-
-# python -m pytest -v --driver Chrome --driver-path /tests_drivers/chromedriver.exe tests/test_all_pets_have_different_name.py
